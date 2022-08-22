f2688b355424c129eb093ae62a73e59d94e2c248
Patrice Côté <pat@patricecote.com>
2022-08-22 09:27:53 -0400

    fake $ settings
    fake $ settings for compatibility with gsender...

--- FluidNC/src/SettingsDefinitions.h
+++ FluidNC/src/SettingsDefinitions.h
@@ -10,6 +10,40 @@ extern StringSetting* start_message;
 
 extern IntSetting* status_mask;
 
-extern IntSetting* sd_fallback_cs;
+extern IntSetting* fake_report_inches;
+
+extern IntSetting* fake_homing_enabled;
+
+extern IntSetting* fake_homing_dir_invert;
+
+extern IntSetting* fake_homing_pulloff;
+
+extern IntSetting* fake_spindle_min;
+
+extern IntSetting* fake_soft_limits;
+
+extern IntSetting* fake_x_accel;
+
+extern IntSetting* fake_y_accel;
+
+extern IntSetting* fake_z_accel;
+
+extern IntSetting* fake_x_max_feed;
+
+extern IntSetting* fake_y_max_feed;
+
+extern IntSetting* fake_z_max_feed;
+
+extern IntSetting* fake_x_resolution;
+
+extern IntSetting* fake_y_resolution;
+
+extern IntSetting* fake_z_resolution;
+
+extern IntSetting* fake_x_max_travel;
+
+extern IntSetting* fake_y_max_travel;
+
+extern IntSetting* fake_z_max_travel;
 
 extern EnumSetting* message_level;
